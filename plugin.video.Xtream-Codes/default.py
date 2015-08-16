97d3ae34610b2d434e299e278da4ed5211724ae7
pipcan3 <iptvmanager@outlook.com>
2015-08-16 07:02:53 +0100

    h
    f

--- plugin.video.Xtream-Codes/default.py
+++ plugin.video.Xtream-Codes/default.py
@@ -1,24 +1,39 @@
 # All Coding Done By Pipcan See http://mykodi.co.uk or facebook http://facebook.co.uk/kodicontrol or skype:pipcan2015
 import urllib,urllib2,sys,re,xbmcplugin,xbmcgui,xbmcaddon,datetime,os
+# V-- Reads The /resources/settings.xml This I The Username --V
 usern=xbmcplugin.getSetting(int(sys.argv[1]), 'usern')
+# V-- Reads The /resources/settings.xml This I The Password --V
 passw=xbmcplugin.getSetting(int(sys.argv[1]), 'passw')
+# V-- Reads The /resources/settings.xml This I The Website --V
 site=xbmcplugin.getSetting(int(sys.argv[1]), 'site')
+# V-- Reads The /resources/settings.xml This I The PORT --V
 port=xbmcplugin.getSetting(int(sys.argv[1]), 'port')
+# V-- Reads The /resources/settings.xml This I The Type --V
 type=xbmcplugin.getSetting(int(sys.argv[1]), 'type')
+# V-- This Is For Web Login Successful Hide The Login Button 
 active=xbmcplugin.getSetting(int(sys.argv[1]), 'active')
+source=xbmcplugin.getSetting(int(sys.argv[1]), 'source')
+# V-- This Is For Web Login Successful Hide The Login Button 
+source2 = 
+'''                          V----The Addon Name----V'''
 ADDON = xbmcaddon.Addon(id='plugin.video.Xtream-Codes')
+'''        V----The Addon Name----V'''
 AddonID = 'plugin.video.Xtream-Codes'
 Addon = xbmcaddon.Addon(AddonID)
 addonDir = Addon.getAddonInfo('path').decode("utf-8")
 #Created By Pipcan.
-BASE = ""
+fetch_url = ""
+date= datetime.datetime.today().strftime('%Y%m')
+time= datetime.datetime.today().strftime('%H%M%S')
 def CATEGORIES():
-        if active =="True":
-            addDir('[COLOR yellow]LOGGED IN AS:[/COLOR]  %s'%(usern),'%s:%s/panel_api.php?username=%s&password=%s'%(site,port,usern,passw),1,'')
+#       V-----------------V This is a function that when a user logges in to reveal bellow directorys
+        if active =="true":
+            addDir('[COLOR yellow]LOGGED IN AS:[/COLOR]  %s'%(usern),'%s:%s/panel_api.php?username=%s&password=%s'%(site,port,usern,passw),6	,'')
             addDir('My Account','%s:%s/panel_api.php?username=%s&password=%s'%(site,port,usern,passw),6,'')
             addDir('Live','%s:%s/panel_api.php?username=%s&password=%s'%(site,port,usern,passw),5,'')
             addDir('Video On-Demand','%s:%s/panel_api.php?username=%s&password=%s'%(site,port,usern,passw),4,'')
             addDir2('Settings','settings',7,'')
+#       V---V If User Not Logged In Show Login Button
         else:
             addDir('Click To Login','%s:%s/panel_api.php?username=%s&password=%s'%(site,port,usern,passw),3,'')
 
@@ -30,19 +45,39 @@ def OPEN_URL(url):
         response.close()
         return link
 
-def API(url):
+def VOD(url):
         vidlocation=('%s:%s/movie/%s/%s/'%(site,port,usern,passw))
         link = OPEN_URL(url)
         match=re.compile('{"name":"(.+?)","stream_id":"(.+?)".+?container_extension":"(.+?)","stream_icon":"(.+?)",".+?"epg_data":.+?plot":(.+?)",".+?releasedate":"(.+?)"').findall(link)
         for name,streamid,container,iconimage,plot,year in match:
                 addLink3('[COLOR yellow]%s[/COLOR]%s'%(name,year),'%s%s.%s'%(vidlocation,streamid,container),'','%s'%(iconimage).replace("\/","/"),'','%s'%(plot))				
+def EPG(name,url):
+        epglocation=xbmcplugin.getSetting(int(sys.argv[1]), 'file')
+        addLink(' %s  [I]Click To Watch[/I]'%(name),'%s'%(url),'','')
+        addLink(' [COLOR green]Programme Ended[/COLOR] - [COLOR red]Programme Not Started[/COLOR]','','','')
+        xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_TITLE )
+        if source == "Downloaded":
+            epglocation2 = 'file:///%s'%(epglocation)
+        else:
+            epglocation2 = ('%s'%(source2))
+        link = OPEN_URL(epglocation2)
+        match = re.compile('start="%s10(.+?)\s.+?".+?channel="%s">.+?\n.+?<title lang=".+?">(.+?)</title>.+?\n.+?<desc lang=".+?">(.+?)<'%(date,name)).findall(link)
+        for start,prog,desc in match:
+                if start > "%s"%(time):
+                    addLink3('%s - [COLOR red]%s[/COLOR] '%(start,prog),'','','','','%s'%(desc))
+                else:
+                    addLink3('%s - [COLOR green]%s[/COLOR] '%(start,prog),'','','','','%s'%(desc))			
+        addLink('zzz[COLOR blue]END OF LISTINGS[/COLOR]','','','')
 def LIVE(url):
+        xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_TITLE )
         vidlocation=('%s:%s/live/%s/%s/'%(site,port,usern,passw))
         link = OPEN_URL(url)
         match=re.compile('{"name":"(.+?)","stream_id":"(.+?)".+?"live":"1".+?icon":"(.+?)"').findall(link)
         for name,url,iconimage in match:
                 addLink('%s'%(name),'%s/%s.ts'%(vidlocation,url,),'%s'%(iconimage).replace("\/","/"),'')        
+
 def ACCOUNT(url):
+        addDir2('[COLOR green]Download EPG[/COLOR]','%s'%(source2),9,'')
         link = OPEN_URL(url)
         match=re.compile('"username":"(.+?)"').findall(link)
         match1=re.compile('"status":"(.+?)"').findall(link)
@@ -52,6 +87,7 @@ def ACCOUNT(url):
         match5=re.compile('"max_connections":"(.+?)"').findall(link)
         match6=re.compile('"allowed_output_formats":(.+?)}').findall(link)
         for url in match:
+                addDir2('[COLOR green]ACCOUNT INFOMATION[/COLOR]','','','')
                 addDir2('[COLOR yellow]Username:                       [/COLOR][I]%s[/I]'%(url),'','','')
         for url in match1:
                 addDir2('[COLOR yellow]Status:                            [/COLOR][I]%s[/I]'%(url),'','','')
@@ -65,46 +101,13 @@ def ACCOUNT(url):
                 addDir2('[COLOR yellow]Max Connection:           [/COLOR][I]%s[/I]'%(url),'','','')
         for url in match6:
                 addDir2('[COLOR yellow]Avalible Formats:          [/COLOR][I]%s[/I]'%(url),'','','')
-
+# This Funtion Is For Opening The Settings Panel
 def openSettings():
     ADDON.openSettings()
-def INDEX(url):
-	if url=="browse?q=":
-	  searchString = addSearch()
-	  url="browse?q="+searchString
-	after = url
-	url = BASE + url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-	try:
-	  appdg = after.split('&')[1]
-	  before = after.split('&')[0]
-	  appdg = int(appdg.split('=')[1]) + 1
-	  newURL = before + "&page=" + str(appdg)
-	except:
-	  newURL = after + "&page=2"
-	  appdg = 2
-	addDir("Go To Page " + str(appdg), newURL, 1, "")
-        match=re.compile('<a href="(.+?)"><img class="thumbnail_image" src="(.+?)" alt="(.+?)"').findall(link)
-        for url,thumbnail,name in match:
-	  req = urllib2.Request(url)
-	  req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-	  response = urllib2.urlopen(req)
-	  link=response.read()
-	  response.close()
-	  match=re.compile('file: "(.+?)",').findall(link)
-	  for url in match:
-                addLink(name,url,thumbnail,"")
-          match=re.compile('src="http://www.youtube.com/embed/(.+?)?rel=0').findall(link)
-          for url in match:
-	    youtubeurl = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % url
-	    addLink(name,youtubeurl,thumbnail,"")
+
 def DownloaderClass(url,dest):
     dp = xbmcgui.DialogProgress()
-    dp.create("Iptv Manager","Downloading")
+    dp.create("Getting EPG","Downloading")
     urllib.urlretrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
 def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
     try:
@@ -117,27 +120,20 @@ def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
     if dp.iscanceled(): 
         print "DOWNLOAD CANCELLED" # need to get this part working
         dp.close()
-def UpdateMe():
+def GRABEPG(url):
     dialog = xbmcgui.Dialog()
-    if dialog.yesno("Update", 'Do you Wish To update Channels','', "",'Close','Yes'):
+    if dialog.yesno("Download EPG", 'Do you Wish To Download EPG','', "",'Close','Yes'):
         dp = xbmcgui.DialogProgress()
-        dp.create('UPDATING')
-        dp.update(20)
-        dialog = xbmcgui.Dialog()
-        dp = xbmcgui.DialogProgress()
-        dp.create('Downloading Zip')
-        dp.update(60)
-        url = "%s:%s/get.php?username=%s&password=%s&type=m3u_plus&output=mpegts"%(site,port,usern,passw)
-        localfile = os.path.join(addonDir,"channels.m3u")
-        urllib.urlretrieve(url,localfile)
-        dp.update(70)
+        dp.create('Downloading EPG')
+        url = ("%s"%(source2))
+        localfile = os.path.join(addonDir,"resources/epg.xml")
+        urllib.urlretrieve(url,localfile,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
         xbmc.executebuiltin("UpdateLocalAddons")
         xbmc.executebuiltin("UpdateAddonRepos")
-        dp.update(100)
         dp.close()
         dialog.ok("All Done", " Update Is Complete")
     else:
-        return                
+        return	
 def VIDEOLINKS(url,name):
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
@@ -203,7 +199,10 @@ def addLink(name,url,iconimage,urlType):
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        menu=[]
+        menu.append(('[COLOR yellow]EPG[/COLOR]','Container.update(%s?mode=8&name=%s&url=%s)'% (sys.argv[0],name,url)))
         liz.setProperty('IsPlayable','true')
+        liz.addContextMenuItems(items=menu, replaceItems=True)
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
         return ok
 
@@ -270,25 +269,32 @@ if mode==None or url==None or len(url)<1:
         CATEGORIES()
        
 elif mode==1:
-        print ""+url
-        INDEX(url)
-        
+    print ""+url
+    INDEX(url)
+
 elif mode==2:
-        print ""+url
-        addSearch()
+    print ""+url
+    addSearch()
+
 elif mode==3:
     SIGNIN()
 
 elif mode==4:
-    API(url)
+    VOD(url)
 
 elif mode==5:
     LIVE(url)
 
 elif mode==6:
     ACCOUNT(url)
+
 elif mode==7:
         openSettings()
+elif mode==8:
+    name=name.replace('%20',' ')
+    EPG(name,url)
+elif mode==9:
+    GRABEPG(url)
 
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
